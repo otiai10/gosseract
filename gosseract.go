b919f33088993c12aebc782103af0221f3d09079
otiai10 <otiai10@gmail.com>
2013-10-30 18:25:54 +0900

    Add func `Anyway`

--- gosseract.go
+++ gosseract.go
@@ -1,5 +1,54 @@
 package gosseract
 
+import (
+  . "os"
+  "os/exec"
+  "io/ioutil"
+)
+
+type AnywayArgs struct {
+  SourcePath  string
+  Destination string
+}
+var (
+  TMPDIR = "/tmp"
+  OUTEXT = ".txt"
+  COMMAND = "tesseract"
+)
+
 func HelloGosseract() string {
   return "Hello,Gosseract!"
+}
+
+/**
+ * とにかくパラメータ喰わせて一発でOCRしたい場合の
+ * コマンドラッパー
+ */
+func Anyway(args AnywayArgs) string {
+  // 最終的な返り値
+  out := ""
+
+  // 引数で行き先を指定されない場合は
+  // (とりあえず) `/tmp/anyway,txt`に置く
+  // tesseractが標準出力に対応してるハズ
+  // tesseractのバージョンを見るようなメソッドを用意しないとアカンなこれ
+  if args.Destination == "" {
+    args.Destination = TMPDIR + "/anyway"
+  }
+
+  // tesseractコマンドを実行
+  command := exec.Command(COMMAND, args.SourcePath, args.Destination)
+  e := command.Run()
+  if e != nil {
+    panic(e)
+  }
+
+  // 出力を読む
+  // tesseractの出力はコマンドラインの第二引数に.txtを付けたものに置かれる
+  fn := args.Destination + OUTEXT
+  f, _ := OpenFile(fn, 1, 1)
+  buf, _ := ioutil.ReadFile(f.Name())
+  out = string(buf)
+
+  return out
 }
--- tests/hello_test.go
+++ tests/hello_test.go
@@ -2,13 +2,26 @@ package gosseract
 
 import (
   "github.com/otiai10/gosseract-ocr"
+  . "github.com/r7kamura/gospel"
   "testing"
 )
 
 func TestHelloGosseract(t *testing.T) {
-  expected := "Hello,Gosseract!"
-  actual := gosseract.HelloGosseract()
-  if actual != expected {
-    t.Errorf("Expected '%v', Actual '%v'", expected, actual)
-  }
+  Describe(t, "HelloGosseract", func() {
+    It("should say \"Hello,Gosseract!\"", func() {
+      Expect(gosseract.HelloGosseract()).To(Equal, "Hello,Gosseract!")
+    })
+  })
+}
+
+func TestAnyway(t *testing.T) {
+  Describe(t, "Anyway", func() {
+    It("should exeute OCR anyway with args.", func() {
+      args := gosseract.AnywayArgs{
+        SourcePath: "samples/png/sample000.png",
+      }
+      out := gosseract.Anyway(args)
+      Expect(out).To(Equal, "01:37:58\n\n");
+    });
+  });
 }

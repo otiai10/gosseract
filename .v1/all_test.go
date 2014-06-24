311d0040b05926ea6aee7588cc399ac74c892cd3
otiai10 <otiai10@gmail.com>
2014-06-24 13:23:51 +0900

    Remove v1

--- .v1/all_test.go
+++
@@ -1,9 +0,0 @@
-package gosseract_test
-
-import "github.com/otiai10/gosseract"
-import . "github.com/otiai10/mint"
-import "testing"
-
-func TestNewClient(t *testing.T) {
-	Expect(t, gosseract.Greet()).ToBe("Hello,Gosseract.")
-}

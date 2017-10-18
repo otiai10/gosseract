f62ca61f2a8f62ca585dcc7a3ca363186c290241
Hiromu OCHIAI <otiai10@gmail.com>
2017-10-18 19:51:55 +0900

    Fix code style

--- tessbridge.cpp
+++ tessbridge.cpp
@@ -2,21 +2,18 @@
 #include <leptonica/allheaders.h>
 #include "tessbridge.h"
 
-TessBaseAPI Init()
-{
+TessBaseAPI Init() {
   tesseract::TessBaseAPI * api = new tesseract::TessBaseAPI();
   return (void*)api;
 }
 
-void Free(TessBaseAPI a)
-{
+void Free(TessBaseAPI a) {
   tesseract::TessBaseAPI * api = (tesseract::TessBaseAPI*)a;
   api->End();
   delete api;
 }
 
-const char* Version(TessBaseAPI a)
-{
+const char* Version(TessBaseAPI a) {
   tesseract::TessBaseAPI * api = (tesseract::TessBaseAPI*)a;
   const char* v = api->Version();
   return v;

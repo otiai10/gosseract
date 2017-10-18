6d726edf9420b7b23f991295790bb9414808ee6b
Hiromu OCHIAI <otiai10@gmail.com>
2017-10-18 19:51:24 +0900

    Remove unnecessary comment

--- all_test.go
+++ all_test.go
@@ -7,10 +7,6 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-	// client, err := NewClient()
-	// Expect(t, err).ToBe(nil)
-	// Expect(t, client).TypeOf("*gosseract.APIClient")
-
 	version := Version()
 	Expect(t, version).ToBe("3.05.00")
 }

8c9fdd29ab1e5d5a4c375cbb711abeae30422670
Anson Liu <ansonl@users.noreply.github.com>
2018-02-11 16:50:14 -0500

    Add functions for HOCR and setting config file

--- tessbridge.h
+++ tessbridge.h
@@ -6,11 +6,13 @@ typedef void* TessBaseAPI;
 TessBaseAPI Create(void);
 void Free(TessBaseAPI);
 void Init(TessBaseAPI, char*, char*);
+void InitConfig(TessBaseAPI, char*, char*, char*);
 void SetVariable(TessBaseAPI, char*, char*);
 void SetImage(TessBaseAPI, char*);
 void SetPageSegMode(TessBaseAPI, int);
 int GetPageSegMode(TessBaseAPI);
 char* UTF8Text(TessBaseAPI);
+char* HOCRText(TessBaseAPI);
 const char* Version(TessBaseAPI);
 
 #ifdef __cplusplus

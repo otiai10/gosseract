ad727cff59ab6ed9cdec256886299249fcb5cc3e
Hiromu Ochiai <otiai10@gmail.com>
2018-06-16 19:01:23 +0900

    Refactor: goodbye `prepare`
    Because it's better to have setters validate inputs,
    and return errors.

--- tessbridge.h
+++ tessbridge.h
@@ -6,13 +6,12 @@ typedef void* TessBaseAPI;
 typedef void* PixImage;
 
 TessBaseAPI Create(void);
+
 void Free(TessBaseAPI);
 void Clear(TessBaseAPI);
 void ClearPersistentCache(TessBaseAPI);
 int Init(TessBaseAPI, char*, char*, char*);
 bool SetVariable(TessBaseAPI, char*, char*);
-PixImage SetImage(TessBaseAPI, char*);
-PixImage SetImageFromBuffer(TessBaseAPI, unsigned char*, int);
 void SetPixImage(TessBaseAPI a, PixImage pix);
 void SetPageSegMode(TessBaseAPI, int);
 int GetPageSegMode(TessBaseAPI);
@@ -20,6 +19,8 @@ char* UTF8Text(TessBaseAPI);
 char* HOCRText(TessBaseAPI);
 const char* Version(TessBaseAPI);
 
+PixImage CreatePixImageByFilePath(char*);
+PixImage CreatePixImageFromBytes(unsigned char*, int);
 void DestroyPixImage(PixImage pix);
 
 #ifdef __cplusplus

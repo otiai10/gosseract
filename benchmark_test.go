c225bf2f118946fb5757f6c0a0d52f0a3662ca38
Jonathan Leftwich <jleftwich@shipt.com>
2020-01-23 14:20:46 -0600

    add capacity to out slice

--- benchmark_test.go
+++ benchmark_test.go
@@ -19,3 +19,21 @@ func BenchmarkClient_Text2(b *testing.B) {
 	}
 	client.Close()
 }
+
+func BenchmarkClient_GetBoundingBoxes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		client := NewClient()
+		client.SetImage("./test/data/003-longer-text.png")
+		client.GetBoundingBoxes(3)
+		client.Close()
+	}
+}
+
+func BenchmarkClient_GetBoundingBoxesVerbose(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		client := NewClient()
+		client.SetImage("./test/data/003-longer-text.png")
+		client.GetBoundingBoxesVerbose()
+		client.Close()
+	}
+}
